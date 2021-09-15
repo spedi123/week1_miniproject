429405ad06397e4b58396961f47997d101a660f5
Marlcocoro <joongyue@gmail.com>
2021-09-15 22:18:39 +0900

    20210915_05

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def sign_in():
     if result is not None:
         payload = {
          'id': id_receive,
-         'exp': datetime.utcnow() + timedelta(seconds= 60 * 5)
+         'exp': datetime.utcnow() + timedelta(seconds= 60 * 5 * 60)
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
 
@@ -67,7 +67,7 @@ def sign_up():
     doc = {
         "id": id_receive,
         "password": password_hash,
-        "attended": ''
+        "attended": []
     }
     db.users.insert_one(doc)
     return jsonify({'result': 'success'})
@@ -90,22 +90,24 @@ def gathering_join():
 
         # 이미 참석하기로 한 모임 리스트, request로 넘겨받은 모임 title 저장
         # str split(',')로 구현 할 예정이라 title에는 ,사용 금지 제약 걸어야
-        str_attended = user_info["attended"]
-        already_attended = str_attended.split(',')
+        attended_receive = user_info["attended"]
+
 
         title_receive = request.form["title_give"]
 
         # 토글로 구현, bool값을 request로 전달해서 구분 - 삭제 요청인지 취소 요청인지
-        is_cancel = request.form["is_cancel_give"]
+        cancel_receive = request.form["is_cancel_give"]
+        is_cancel = int(cancel_receive)
+
 
         # 삭제 요청시(조건문 필요 없음)
-        if is_cancel:
-            temp_attended = user_info["attended"]
-            temp_attended_list = temp_attended.split(',')
-            temp_attended_list.remove(title_receive)
-            temp_attended = ','.join(temp_attended_list)
-            db.users.update_one({'id': payload["id"]}, {'$set': {'joined': temp_attended}})
-            db.gathering_data.delete_one({'id': user_info['id']}, {'gathering_title': title_receive})
+        if is_cancel == 1:
+            if title_receive in attended_receive:
+                attended_receive.remove(title_receive)
+            else:
+                return jsonify({'result' : 'success', "msg" : "참석한 모임이 아닙니다."})
+            db.users.update_one({'id': payload["id"]}, {'$set': {'attended': attended_receive}})
+            db.gathering_data.delete_one({'id': user_info['id'], 'title' : title_receive})
 
             # 모임 정보 db, 유저 정보의 참석 리스트에서 각각 데이터값 삭제 완료 후 jsonify 리턴
             return jsonify({'result' : 'success', "msg" : "모임 참석 취소 완료!"})
@@ -117,28 +119,33 @@ def gathering_join():
         # gatherings db에서 좀 전에 받아온 user db의 attended 배열에서 해당하는 값을 찾음
         # 찾은 값을 already_attended_time에 저장
         already_attended_time = []
-        for i in already_attended:
-            already_attended_time.append(db.gatherings.find_one({"title" : i})["date"])
+        print(len(attended_receive))
+        if len(attended_receive) == 0:
+            attended_receive.append(title_receive)
+            db.users.update_one({'id': payload["id"]}, {'$set': {'attended': attended_receive}})
+            doc = {
+                "title": title_receive,
+                "id": user_info["id"]
+            }
+            db.gathering_data.insert_one(doc)
+            return jsonify({"result": "success", 'msg': '조인 완료'})
+        else:
+            for i in attended_receive:
+                already_attended_time.append(db.gatherings.find_one({"title" : i})["date"])
 
-        # 중복값 찾기
         for a in already_attended_time:
             if a == new_appointment_time:
                 #참석 불가, 리턴
                 return jsonify({"result": "success", 'msg': '이미 해당 시간에 약속이 존재합니다.'})
 
         # 이후 각 db에 저장 후
-        temp_attended = user_info["attended"]
-        if len(temp_attended) >= 1:
-            temp_attended.join(',').join(title_receive)
-        else :
-            temp_attended.join(title_receive)
-        db.users.update_one({'id': payload["id"]}, {'$set': {'joined': temp_attended}})
-
+        attended_receive.append(title_receive)
+        db.users.update_one({'id': payload["id"]}, {'$set': {'attended': attended_receive}})
         doc = {
             "title": title_receive,
             "id": user_info["id"]
         }
-        db.post_data.insert_one(doc)
+        db.gathering_data.insert_one(doc)
         return jsonify({"result" : "success", 'msg' : '조인 완료'})
 
 
